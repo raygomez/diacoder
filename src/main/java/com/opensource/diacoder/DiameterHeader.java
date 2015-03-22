9cb2e5426e895c346194981535eba04569550c66
Ray Gomez <rayvincent.gomez@gmail.com>
2015-03-22 22:35:31 +0800

    added main class and basic DiameterHeader class

---
+++ src/main/java/com/opensource/diacoder/DiameterHeader.java
@@ -0,0 +1,23 @@
+package com.opensource.diacoder;
+
+public class DiameterHeader {
+
+	int version;
+	int messageLength;
+	boolean isRequest;
+	boolean isProxiable;
+	boolean isError;
+	boolean isRetransmitted;
+	int commandCode;
+	int applicationId;
+	int hopByHopId;
+	int endToEndId;
+	
+	public DiameterHeader() {
+		
+	}
+	
+	public DiameterHeader(byte[] array){
+		
+	}
+}
