49f7754946789521a225b28b175f441a442687fe
Ray Gomez <rayvincent.gomez@gmail.com>
2015-03-24 08:38:07 +0800

    added header encode

--- src/main/java/com/opensource/diacoder/DiameterHeader.java
+++ src/main/java/com/opensource/diacoder/DiameterHeader.java
@@ -2,24 +2,24 @@ package com.opensource.diacoder;
 
 public class DiameterHeader {
 
-	int version;
-	int messageLength;
-	boolean isRequest;
-	boolean isProxiable;
-	boolean isError;
-	boolean isRetransmitted;
-	int commandCode;
-	int applicationId;
-	int hopByHopId;
-	int endToEndId;
-	
+	private byte version;
+	private int messageLength;
+	private boolean isRequest;
+	private boolean isProxiable;
+	private boolean isError;
+	private boolean isRetransmitted;
+	private int commandCode;
+	private int applicationId;
+	private int hopByHopId;
+	private int endToEndId;
+
 	public DiameterHeader() {
-		
+
 	}
-	
-	public DiameterHeader(byte[] input){
+
+	public DiameterHeader(byte[] input) {
 		decode(input);
-		
+
 	}
 
 	public int getVersion() {
@@ -50,15 +50,56 @@ public class DiameterHeader {
 		return commandCode;
 	}
 
-	public int getApplicationId(){
+	public int getApplicationId() {
 		return applicationId;
 	}
+
 	public int getHopByHopId() {
 		return hopByHopId;
 	}
 
 	public int getEndToEndId() {
 		return endToEndId;
+	}
+
+	public void setVersion(byte version) {
+		this.version = version;
+	}
+
+	public void setMessageLength(int messageLength) {
+		this.messageLength = messageLength;
+	}
+
+	public void setRequest(boolean isRequest) {
+		this.isRequest = isRequest;
+	}
+
+	public void setProxiable(boolean isProxiable) {
+		this.isProxiable = isProxiable;
+	}
+
+	public void setError(boolean isError) {
+		this.isError = isError;
+	}
+
+	public void setRetransmitted(boolean isRetransmitted) {
+		this.isRetransmitted = isRetransmitted;
+	}
+
+	public void setCommandCode(int commandCode) {
+		this.commandCode = commandCode;
+	}
+
+	public void setApplicationId(int applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public void setHopByHopId(int hopByHopId) {
+		this.hopByHopId = hopByHopId;
+	}
+
+	public void setEndToEndId(int endToEndId) {
+		this.endToEndId = endToEndId;
 	}
 
 	public void decode(byte[] input) {
@@ -69,13 +110,40 @@ public class DiameterHeader {
 		isProxiable = (input[4] & 0x40) > 0;
 		isError = (input[4] & 0x20) > 0;
 		isRetransmitted = (input[4] & 0x10) > 0;
-		commandCode = input[5] << 16 | input[6] << 8 | input [7];
-		applicationId = input[8] << 24 | (input[9] & 0xFF) << 16 | (input[10] & 0xFF) << 8 | input [11];
-		hopByHopId =  input[12] << 24 | (input[13] & 0xFF) << 16 | (input[14] & 0xFF) << 8 | input [15];
-		endToEndId =  input[16] << 24 | (input[17] & 0xFF) << 16 | (input[18] & 0xFF) << 8 | input [19];
+		commandCode = input[5] << 16 | input[6] << 8 | input[7];
+		applicationId = input[8] << 24 | (input[9] & 0xFF) << 16
+				| (input[10] & 0xFF) << 8 | input[11];
+		hopByHopId = input[12] << 24 | (input[13] & 0xFF) << 16
+				| (input[14] & 0xFF) << 8 | input[15];
+		endToEndId = input[16] << 24 | (input[17] & 0xFF) << 16
+				| (input[18] & 0xFF) << 8 | input[19];
 	}
 
-	public void encode(byte[] input){
-		
+	public void encode(byte[] input) {
+
+		input[0] = version;
+		input[1] = (byte) ((messageLength >> 16) & 0xFF);
+		input[2] = (byte) ((messageLength >> 8) & 0xFF);
+		input[3] = (byte) (messageLength & 0xFF);
+		input[4] = (byte) (isRequest ? 0x80 : 0);
+		input[4] |= (byte) (isProxiable ? 0x40 : 0);
+		input[4] |= (byte) (isError ? 0x20 : 0);
+		input[4] |= (byte) (isRetransmitted ? 0x10 : 0);
+		input[5] = (byte) ((commandCode >> 16) & 0xFF);
+		input[6] = (byte) ((commandCode >> 8) & 0xFF);
+		input[7] = (byte) (commandCode & 0xFF);
+		input[8] = (byte) ((applicationId >> 24) & 0xFF);
+		input[9] = (byte) ((applicationId >> 16) & 0xFF);
+		input[10] = (byte) ((applicationId >> 8) & 0xFF);
+		input[11] = (byte) (applicationId & 0xFF);
+		input[12] = (byte) ((hopByHopId >> 24) & 0xFF);
+		input[13] = (byte) ((hopByHopId >> 16) & 0xFF);
+		input[14] = (byte) ((hopByHopId >> 8) & 0xFF);
+		input[15] = (byte) (hopByHopId & 0xFF);
+		input[16] = (byte) ((endToEndId >> 24) & 0xFF);
+		input[17] = (byte) ((endToEndId >> 16) & 0xFF);
+		input[18] = (byte) ((endToEndId >> 8) & 0xFF);
+		input[19] = (byte) (endToEndId & 0xFF);
+
 	}
 }
--- src/test/java/com/opensource/diacoder/DiameterHeaderTest.java
+++ src/test/java/com/opensource/diacoder/DiameterHeaderTest.java
@@ -9,12 +9,12 @@ public class DiameterHeaderTest {
 	@Test
 	public void testDecodeHeader() {
 
-		byte[] input = { 0x01, 0x00, 0x00, 0x10, (byte) 0xC0, 0x00, 0x00, 0x00,
+		byte[] input = { 0x01, 0x00, 0x00, 0x14, (byte) 0xC0, 0x00, 0x00, 0x00,
 				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
 				0x00, 0x02 };
 		DiameterHeader header = new DiameterHeader(input);
 		assertEquals(0x01, header.getVersion());
-		assertEquals(16, header.getMessageLength());
+		assertEquals(20, header.getMessageLength());
 		assertTrue(header.isRequest());
 		assertTrue(header.isProxiable());
 		assertFalse(header.isError());
@@ -29,14 +29,14 @@ public class DiameterHeaderTest {
 	@Test
 	public void testDecodeHeaderWithOtherValues() {
 
-		byte[] input = { (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x10,
-				(byte) 0x30, (byte) 0x11, (byte) 0x22, (byte) 0x33,
-				(byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44,
-				(byte) 0x99, (byte) 0x88, (byte) 0x77, (byte) 0x66,
-				(byte) 0x55, (byte) 0x44, (byte) 0x33, (byte) 0x22 };
+		byte[] input = { 0x01, 0x00, 0x00, 0x14,
+				0x30, 0x11, 0x22, 0x33,
+				0x11, 0x22, 0x33, 0x44,
+				(byte) 0x99, (byte) 0x88, 0x77, 0x66,
+				0x55, 0x44, 0x33, 0x22 };
 		DiameterHeader header = new DiameterHeader(input);
 		assertEquals(0x01, header.getVersion());
-		assertEquals(16, header.getMessageLength());
+		assertEquals(20, header.getMessageLength());
 		assertFalse(header.isRequest());
 		assertFalse(header.isProxiable());
 		assertTrue(header.isError());
@@ -46,5 +46,29 @@ public class DiameterHeaderTest {
 		assertEquals(0x99887766, header.getHopByHopId());
 		assertEquals(0x55443322, header.getEndToEndId());
 
+	}
+
+	@Test
+	public void testEncodeHeader() {
+		byte[] expected = { 0x01, 0x00, 0x00, 0x14, (byte) 0xC0, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+				0x00, 0x02 };
+		DiameterHeader header = new DiameterHeader();
+		header.setVersion((byte) 1);
+		header.setMessageLength(20);
+		header.setRequest(true);
+		header.setProxiable(true);
+		header.setError(false);
+		header.setRetransmitted(false);
+		header.setCommandCode(0);
+		header.setApplicationId(0);
+		header.setHopByHopId(1);
+		header.setEndToEndId(2);
+		
+		byte[] result = new byte[expected.length];
+		header.encode(result);
+		
+		assertArrayEquals(expected, result);
+		
 	}
 }
