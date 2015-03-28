576c2401f5d4a9ff1560481d0c475ef344a28d67
Ray Gomez <rayvincent.gomez@gmail.com>
2015-03-28 12:07:56 +0800

    refactor AvpHeader

--- src/main/java/com/opensource/diacoder/AvpHeader.java
+++ src/main/java/com/opensource/diacoder/AvpHeader.java
@@ -6,7 +6,9 @@ public class AvpHeader {
 	private boolean hasVendorId;
 	private boolean isMandatory;
 	private boolean isEncrypted;
+	private int avplength;
 	private int vendorId;
+
 	
 	public AvpHeader() {
 	}
@@ -19,27 +21,19 @@ public class AvpHeader {
 		this.avpCode = avpCode;
 	}
 
-	public boolean isVendorBit() {
-		return hasVendorId;
-	}
-
-	public void setVendorBit(boolean isVBit) {
-		this.hasVendorId = isVBit;
-	}
-
-	public boolean isMandatoryBit() {
+	public boolean isMandatory() {
 		return isMandatory;
 	}
 
-	public void setMandatoryBit(boolean isMBit) {
+	public void setMandatory(boolean isMBit) {
 		this.isMandatory = isMBit;
 	}
 
-	public boolean isEncryptedBit() {
+	public boolean isEncrypted() {
 		return isEncrypted;
 	}
 
-	public void setEncryptedBit(boolean isPBit) {
+	public void setEncrypted(boolean isPBit) {
 		this.isEncrypted = isPBit;
 	}
 
@@ -49,6 +43,42 @@ public class AvpHeader {
 
 	public void setVendorId(int vendorId) {
 		this.vendorId = vendorId;
+	}
+
+	public int getAvpLength() {
+		return avplength;
+	}
+
+	public void setAvpLength(int length) {
+		this.avplength = length;
+	}
+
+	public boolean hasVendorId() {
+		return hasVendorId;
+	}
+
+	public void hasVendorId(boolean hasVendorId) {
+		this.hasVendorId = hasVendorId;
+	}
+
+	public void decode(byte[] input) {
+		
+		avpCode = input[0] << 24 | input[1] << 16 | input[2] << 8 | input[3];
+		hasVendorId = (input[4] & 0x80) > 0;
+		isMandatory = (input[4] & 0x40) > 0;
+		isEncrypted = (input[4] & 0x20) > 0;
+		
+		avplength = input[5] << 16 | input[6] << 8 | input[7];  
+		
+		if(hasVendorId()){
+			vendorId = input[8] << 24 | input[9] << 16 | input[10] << 8 | (input[11] & 0xFF);
+		} else {
+			vendorId = 0;
+		}
+	}
+	
+	public void encode(byte[] input) {
+		
 	}
 
 }
--- src/test/java/com/opensource/diacoder/AvpHeaderTest.java
+++ src/test/java/com/opensource/diacoder/AvpHeaderTest.java
@@ -1,6 +1,39 @@
 package com.opensource.diacoder;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class AvpHeaderTest {
 
+	@Test
+	public void testDecodeHeaderNoVendorId() {
+		byte[] input = { 0x00, 0x01, 0x23, 0x45, (byte) 0x40, 0x00, 0x00, 0x08 };
+
+		AvpHeader header = new AvpHeader();
+		header.decode(input);
+
+		assertEquals(0x12345, header.getAvpCode());
+		assertFalse(header.hasVendorId());
+		assertTrue(header.isMandatory());
+		assertFalse(header.isEncrypted());
+		assertEquals(0x08, header.getAvpLength());
+	}
+
+	@Test
+	public void testDecodeHeaderVendorId() {
+		byte[] input = { 0x00, 0x01, 0x23, 0x45, (byte) 0xC0, 0x00, 0x00, 0x0C,
+				0x00, 0x00, 0x28, (byte) 0xAF };
+
+		AvpHeader header = new AvpHeader();
+		header.decode(input);
+
+		assertEquals(0x12345, header.getAvpCode());
+		assertTrue(header.hasVendorId());
+		assertEquals(10415, header.getVendorId());
+		assertTrue(header.isMandatory());
+		assertFalse(header.isEncrypted());
+		assertEquals(12, header.getAvpLength());
+		
+	}
 }
