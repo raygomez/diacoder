94739d772498f1a061c0e70a20bab0ebc65da676
Ray Gomez <rayvincent.gomez@gmail.com>
2015-03-29 15:39:06 +0800

    added Launcher

---
+++ src/main/java/com/opensource/diacoder/Launcher.java
@@ -0,0 +1,13 @@
+package com.opensource.diacoder;
+
+public class Launcher {
+
+	public Launcher() {
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/com/opensource/diacoder/LauncherTest.java
@@ -0,0 +1,6 @@
+package com.opensource.diacoder;
+
+
+public class LauncherTest {
+
+}
