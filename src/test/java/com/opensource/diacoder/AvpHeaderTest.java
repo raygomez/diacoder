587eeca03774f01c2850ac4723b4de79576c9336
Ray Gomez <rayvincent.gomez@gmail.com>
2015-03-29 15:45:35 +0800

    added new avpheader test

--- src/test/java/com/opensource/diacoder/AvpHeaderTest.java
+++ src/test/java/com/opensource/diacoder/AvpHeaderTest.java
@@ -21,6 +21,22 @@ public class AvpHeaderTest {
 	}
 
 	@Test
+	public void testEncodeHeaderNoVendorId(){
+		byte[] expected = { 0x00, 0x01, 0x23, 0x45, (byte) 0x40, 0x00, 0x00, 0x08 };
+		byte[] result = new byte[expected.length];
+		
+		AvpHeader header = new AvpHeader();
+		header.setAvpCode(0x12345);
+		header.setMandatory(true);
+		header.setEncrypted(false);
+		header.hasVendorId(false);
+		header.setAvpLength(8);
+		header.encode(result);
+
+		assertArrayEquals(expected, result);
+		
+	}
+	@Test
 	public void testDecodeHeaderVendorId() {
 		byte[] input = { 0x00, 0x01, 0x23, 0x45, (byte) 0xC0, 0x00, 0x00, 0x0C,
 				0x00, 0x00, 0x28, (byte) 0xAF };
