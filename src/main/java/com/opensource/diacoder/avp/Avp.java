2b20cb722787df38709ac7c0958c2216f8416bc6
Ray Gomez <rayvincent.gomez@gmail.com>
2015-04-19 23:44:14 +0800

    added setters and getters for avp

--- src/main/java/com/opensource/diacoder/avp/Avp.java
+++ src/main/java/com/opensource/diacoder/avp/Avp.java
@@ -2,12 +2,29 @@ package com.opensource.diacoder.avp;
 
 public abstract class Avp {
 
-	private AvpHeader header;
+	protected AvpHeader header;
 	
 	public Avp() {
 	}
-
-	public abstract void encode();
 	
-	public abstract void decode();
+	public void setAvpCode(int avpCode){
+		header.setAvpCode(avpCode);
+	}
+	
+	public void setVendorId(int vendorId){
+		header.setVendorId(vendorId);
+		header.hasVendorId(true);
+	}
+	
+	public void setMandatory(boolean isMandatory){
+		header.setMandatory(isMandatory);
+	}
+	
+	public void setEncryption(boolean isEncrypted){
+		header.setEncrypted(isEncrypted);
+	}
+	
+	public abstract void encode(byte[] output);
+	
+	public abstract void decode(byte[] input);
 }
