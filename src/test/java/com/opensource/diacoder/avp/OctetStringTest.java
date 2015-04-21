cc6e96f3acf48adcaefae46bdb474a7c1306103a
Ray Gomez <rayvincent.gomez@gmail.com>
2015-04-21 22:10:20 +0800

    added test

--- src/test/java/com/opensource/diacoder/avp/OctetStringTest.java
+++ src/test/java/com/opensource/diacoder/avp/OctetStringTest.java
@@ -23,4 +23,24 @@ public class OctetStringTest {
 		assertArrayEquals(expected, actual);
 
 	}
+	
+	@Test
+	public void testOctetStringEncodeWithVendorId() {
+
+		OctetString avp = new OctetString();
+		avp.setAvpCode(0x11223344);
+		avp.setVendorId(0xFFEEDDCC);
+		byte[] data = { (byte) 0x88, 0x77};
+		avp.setData(data);
+		avp.setMandatory(false);
+
+		byte[] expected = { 0x11, 0x22, 0x33, 0x44, (byte) 0x80, 0x00, 0x00,
+				(byte) 0x0E, (byte) 0xFF, (byte) 0xEE, (byte) 0xDD,
+				(byte) 0xCC, (byte) 0x88, 0x77, 0x00, 0x00 };
+		byte[] actual = avp.encode();
+
+		assertArrayEquals(expected, actual);
+
+	}
+
 }
